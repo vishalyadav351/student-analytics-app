e6c08b74fd97e318de34eceb59189a91c66998f3
Vishal Yadav <vishuydv991@gmail.com>
2026-08-10 21:51:31 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,3 +1,9 @@
-from dashbord import server
+import sys
+import os
 
-app = server
+# Root directory ko path me add karna
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from dashbord import server as application
+
+app = application
